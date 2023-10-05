c18c942857f04447fc759f779784759d546db78b
FabCode67 <mwanafunzifabrice@gmail.com>
2023-10-05 02:13:37 +0200

    tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
 function App() {
 
   return (
-   <div className="App bg-red-700 h-screen w-full items-center justify-center m-auto flex text-6xl font-bold">
+   <div className="App bg-amber-100 h-screen w-full items-center justify-center m-auto flex text-6xl font-bold">
     Eglise
     </div>
   )
